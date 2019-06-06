829e61a225606a0b83cee08e4323c3e97d3a6b71
shar09 <sharathtelu9@gmail.com>
2019-06-05 20:39:14 -0500

    object created for all the Q&A's and images

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+var trivia = {
+    q1: "What is Earth's largest Continent?",
+    o1: ["ASIA", "AFRICA", "EUROPE", "ANTARCTICA"],
+    a1: "Asia - Earth’s largest continent at approximately 17,300,000 square miles.",
+    img1: "assets/images/image-1.png",
+
+    q2: "What country has the most natural lakes?",
+    o2: ["UNITED STATES", "AUSTRALIA", "INDIA", "CANADA"],
+    a2: "Canada - It has more than half of all the natural lakes in the world.",
+    img2: "assets/images/image-2.jpg",
+
+    q3: "What is the driest place on Earth?",
+    o3: ["ATACAMA DESERT", "MCMURDO, ANTARCTICA", "SAHARA DESERT", "KUFRA, LIBYA"],
+    a3: "The McMurdo Dry Valleys in Antarctica - It hasn't rained there for more than 2 million years!",
+    img3: "assets/images/image-3.jpg",
+
+    q4: "Which African nation has the most Pyramids?",
+    o4: ["EGYPT", "ALGERIA", "SUDAN", "LIBYA"],
+    a4: "Sudan - Home to over 200 pyramids.",
+    img4: "assets/images/image-4.jpg",
+
+    q5: "What is the oldest city in the world?",
+    o4: ["JERUSALEM", "DAMASCUS", "JERICHO", "ATHENS"],
+    a4:  "Damascus - The capital of Syria, dating back to at least 11,000 years.",
+    img5: "assets/images/image5.jpg",
+
+    q6: "What is the smallest independent country on Earth?" ,
+    o4: ["NAURA", "GRENADA", "MONACO", "VATICAN CITY"],
+    a6: "Vatican City - The world's smallest country with an area of just 0.44 kilometers.",
+    img6: "assets/images/image-6.jpg",
+
+    q7: "What is the major city located on two continents?",
+    o7: ["NEW DELHI", "LONDON", "ROME", "ISTANBUL"],
+    a7: "Istanbul - The only major city located on two continents -- Europe and Asia.",
+    img7: "assets/images/image-7.jpg",
+
+    q8: "What is the largest country in South America?",
+    o8: ["ARGENTINA", "BRAZIL", "PERU", "COLUMBIA"],
+    a8: "Brazil - Covers nearly half of South America and is larger than the United States of America.",
+    img8: "assets/images/image-8.jpg",
+
+    q9: "What is the tallest mountain in the world?",
+    o9: ["QOGIR", "MOUNT EVEREST", "MOUNT KILAMANJARO", "ACONCAGUA"],
+    a9: "Mount Everest - At 8,850 meters (29,035 ft) is the tallest mountain in the world.",
+    img9: "assets/images/image-9.jpg",
+
+    q10: "Which US state has the most active volcanoes?",
+    o10: ["CALIFORNIA", "ALASKA", "WASHINGTON", "HAWAII"],
+    a10: "Alaska - 130 of the 169 volcanoes in the United States  are located here.",
+    img10: "assets/images/image-10.jpg",
+};
